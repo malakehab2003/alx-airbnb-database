060baefa29a4f8bde456e3e0c4857227b2229f37
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2024-11-30 20:15:42 +0200

    update joins_queries.sql add order to the query

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -21,7 +21,8 @@ SELECT
   Review.comment AS Comment
 FROM Property
 LEFT JOIN Review
-ON Property.id = Review.property_id;
+ON Property.id = Review.property_id
+ORDER BY Property.name;
 
 -- using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
 SELECT 
